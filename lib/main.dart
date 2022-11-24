c25c965b283aa6a6aed0fb6f651cd4ce1112db12
Olivier Mul <OlivierJMul7@iCloud.com>
2022-11-24 06:41:22 +0100

    Splash screen
    Het splash werkt op alle platformen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:prosus_final_app/home_screen.dart';
-import 'package:prosus_final_app/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() {runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
