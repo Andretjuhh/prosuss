8ce28595809a4851c430a38491473b1ab78f19b3
TheRealAJ2002 <ajdemaesschalck@gmail.com>
2022-12-06 14:34:38 +0100

    ik heb containers met schaduw en scroll mogelijkheid gemaakt in het scherm dashboard

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -6,24 +6,144 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Dashboard', 
-            style: TextStyle(
-                fontSize: 30
-            ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center ,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("Recente cijfers",),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("niveau",),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("agenda van vandaag",),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("status boeken",),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("overig, test scrollen",),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("overig, test scrollen",),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          offset: Offset(0, 3)
+                      )
+                    ]
+                ),
+                child: Text("overig, test scrollen",),
+              ),
+
+              MaterialButton(onPressed: (){
+                FirebaseAuth.instance.signOut();
+              },
+                color: Color(0xff2b70cf),
+                child: Text('Sign out'),
+              ),
+            ],
           ),
-          MaterialButton(onPressed: (){
-            FirebaseAuth.instance.signOut();
-          },
-            color: Color(0xff2b70cf),
-            child: Text('Sign out'),
-          ),
-        ],
+        ),
       ),
+
     );
   }
 }
