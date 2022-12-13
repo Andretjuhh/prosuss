4dd1ebdf79c05ad505998c1eb5de29ec3ee88b1d
Andretjuhh <andre.silvareis12@gmail.com>
2022-12-13 21:55:21 +0100

    Update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,7 +12,8 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();}
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
 class _HomeScreenState extends State<HomeScreen> {
   final user = FirebaseAuth.instance.currentUser!;
@@ -30,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
     DashboardPage(),
     VakkenPage(),
     VoortgangPage(),
-];
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -42,30 +43,27 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Image.asset('images/White-Blue_Brand.png', height: 180),
       ),
       endDrawer: Drawer(
-
         backgroundColor: Colors.transparent,
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
               decoration: BoxDecoration(
-                  color: Color(0xfff8fbff),
-               ),
-              child: Image.asset(
-                'images/White-Blue_Brand.png',
-                height: 180
+                color: Color(0xfff8fbff),
               ),
+              child: Image.asset('images/White-Blue_Brand.png', height: 180),
             ),
             ListTile(
               title: Text(
-                  'Help',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Color(0xffffffff),
-                  ),
+                'Help',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xffffffff),
+                ),
               ),
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
                   return const HelpPage();
                 }));
               },
@@ -73,40 +71,44 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
+
+
+      //Navigation Menu on the bottom of the screen
+      //Do not change!
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Color(0xfff8fbff),
-        selectedItemColor: Color(0xff000000),
-        currentIndex: _selectedIndex,
-        onTap: _navigateBottomBar,
-        items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: const Icon(SFSymbols.book),
-          activeIcon: Icon(SFSymbols.book_fill),
-          label: 'Boeken',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(SFSymbols.pencil_circle),
-          activeIcon: Icon(SFSymbols.pencil_circle_fill),
-          label: 'Oefenen',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(SFSymbols.house),
-          activeIcon: Icon(SFSymbols.house_fill),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(SFSymbols.square_list),
-          activeIcon: Icon(SFSymbols.square_list_fill),
-          label: 'Vakken',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(SFSymbols.chart_bar),
-          activeIcon: Icon(SFSymbols.chart_bar_fill),
-          label: 'Voortgang',
-        ),
-      ]),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color(0xfff8fbff),
+          selectedItemColor: Color(0xff000000),
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomBar,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: const Icon(SFSymbols.book),
+              activeIcon: Icon(SFSymbols.book_fill),
+              label: 'Boeken',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(SFSymbols.pencil_circle),
+              activeIcon: Icon(SFSymbols.pencil_circle_fill),
+              label: 'Oefenen',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(SFSymbols.house),
+              activeIcon: Icon(SFSymbols.house_fill),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(SFSymbols.square_list),
+              activeIcon: Icon(SFSymbols.square_list_fill),
+              label: 'Vakken',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(SFSymbols.chart_bar),
+              activeIcon: Icon(SFSymbols.chart_bar_fill),
+              label: 'Voortgang',
+            ),
+          ]),
     );
   }
 }
